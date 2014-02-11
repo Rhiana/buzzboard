50b8b772494afebb8481864a9231f33fa93aa61d
Rhiana Heath <rhiana@heath.cc>
2014-02-11 19:10:16 +1100

    started buzzboard rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buzzboard::Application.config.secret_key_base = 'ce0ba97dc4e80345ea6f59c3017cf3fab54c6792acd48ce80b9a1c42d6cdf5b72d4a547b489679c7e5dc6981e00e0b94b6bbbfd429860fdc1c7dd05926df220f'
