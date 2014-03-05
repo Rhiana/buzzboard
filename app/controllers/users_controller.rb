21746688444bad51eb739f565d6616535a158127
Rhiana Heath <rhiana@heath.cc>
2014-03-05 22:11:54 +1100

    addes users controller and updated readme

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Buzzboard::Application.routes.draw do
+  get "users/show"
+  get "users/edit"
+  get "users/index"
+  get "users/new"
+  get "users/create"
+  get "users/update"
+  get "users/destroy"
+  get "usersindex/new"
+  get "usersindex/create"
+  get "usersindex/update"
+  get "usersindex/destroy"
   resources :topics
 
   # See how all your routes lay out with "rake routes".
