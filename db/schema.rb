5eeddb568737da1864b77e9821c4b4ddc135e9a0
Rhiana Heath <rhiana@heath.cc>
2014-03-05 21:43:21 +1100

    Added users model and table, updated readme

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211083853) do
+ActiveRecord::Schema.define(version: 20140305103633) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.string   "interest"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
